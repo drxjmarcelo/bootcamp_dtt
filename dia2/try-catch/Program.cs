364428e3325b3ae1e1ba8b58b9e475396a005a06
Your Name <mendoncajoaom6@gmail.com>
2026-01-29 16:22:24 -0300

    update remover produto

--- dia2/try-catch/Program.cs
+++ dia2/try-catch/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
 
 
 class Program
@@ -10,6 +15,7 @@ class Program
         List<Produto> estoque = new List<Produto>();
 
         string opcao;
+        string opcao1;
 
         do
         {
@@ -45,16 +51,50 @@ class Program
                 Console.WriteLine("Erro: Número fora do limite permitido!");
             }
             
-            Console.WriteLine("\nDeseja cadastrar um novo produto? (s/n): ");
+            Console.WriteLine("\nDeseja cadastrar mais algum produto? (s/n): ");
             opcao = Console.ReadLine().ToLower();
             
         }
         while (opcao == "s");
+        
         Console.WriteLine($"\nProdutos totais no estoque: {estoque.Count}");
         Console.WriteLine("\nProdutos no estoque: ");
         foreach(var produto in estoque)
         {
             Console.WriteLine($"Nome: {produto.Nome} - Preco: {produto.Preco} - Quantidade: {produto.Quantidade}");
+        } 
+
+        Console.WriteLine("\nDeseja remover algum produto? (s/n)");
+        opcao1 = Console.ReadLine().ToLower();
+
+        while (opcao1 == "s")
+        {
+            if (estoque.Count == 0)
+            {
+                Console.WriteLine("Estoque vazio! Não há produtos para remover.");
+                break;
+            }
+
+            for (int i = 0; i < estoque.Count; i++)
+            {
+                Console.WriteLine($"{i} - Nome: {estoque[i].Nome} - Preco: {estoque[i].Preco} - Quantidade: {estoque[i].Quantidade} ");
+            }
+
+            Console.WriteLine("\nDigite o número do produto que deseja remover:");
+            int indice = Convert.ToInt32(Console.ReadLine());
+
+            if (indice >= 0 && indice < estoque.Count)
+            {
+                estoque.RemoveAt(indice);
+                Console.WriteLine("Produto removido com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Índice inválido!");
+            }
+            
+            Console.WriteLine("\nDeseja remover mais algum produto? (s/n)");
+            opcao1 = Console.ReadLine().ToLower();
         }
 
         Console.WriteLine("Pressioe ENTER para sair.");
--- dia2/try-catch/obj/Debug/net10.0/try-catch.AssemblyInfo.cs
+++ dia2/try-catch/obj/Debug/net10.0/try-catch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("try-catch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c1f48932baf7626cafafb7397cb659abd5fc28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac2260fa40af37c751d1f9b68f5d11399f58008")]
 [assembly: System.Reflection.AssemblyProductAttribute("try-catch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("try-catch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
