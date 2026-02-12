6b16936b43853e6ed2ccc6a550365c6b0fcf5e14
drxjmarcelo <mendoncajoaom6@gmail.com>
2026-02-12 17:28:21 -0300

    LoteService added

--- MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
+++ MinhaApi/obj/Debug/net10.0/MinhaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e5a38c0e8a24c796a87419a4fb30025baf105e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f130f6d0bf8b197ad0c1de67404998031b9a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
